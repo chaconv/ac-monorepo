89b8f6d661c0337218e7f6fce7ffc61ab10a55b0
chaconv <alexroolz@gmail.com>
2021-12-06 10:21:58 +0100

    feature(core): integrate components in angular

--- libs/components/stencil.config.ts
+++ libs/components/stencil.config.ts
@@ -1,8 +1,13 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget as angular } from '@stencil/angular-output-target';
 
 export const config: Config = {
   namespace: 'components',
   outputTargets: [
+    angular({
+      componentCorePackage: '@ac/components',
+      directivesProxyFile: '../../apps/angular-seed/src/lib/stencil-generated/components.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
